55848e340d540e56d98ac53d22ace2358f04471d
Nofrialdi <aldi.fasive@gmail.com>
2023-09-01 15:26:41 +0700

    all file

---
+++ src/Details.js
@@ -0,0 +1,169 @@
+// Enter all your detials in this file
+// Logo images
+import logogradient from './assets/logo.svg';
+import logo from './assets/logo2.svg';
+// Profile Image
+import profile from './assets/profile.jpg';
+// Tech stack images
+import html from './assets/techstack/html.png';
+import css from './assets/techstack/css.png';
+import js from './assets/techstack/js.png';
+import react from './assets/techstack/react.png';
+import tailwind from './assets/techstack/tailwind.png';
+import bootstrap from './assets/techstack/bootstrap.png';
+import vscode from './assets/techstack/vscode.png';
+import github from './assets/techstack/github.png';
+import git from './assets/techstack/git.png';
+import npm from './assets/techstack/npm.png';
+import postman from './assets/techstack/postman.png';
+import figma from './assets/techstack/figma.png';
+// Porject Images
+import projectImage1 from './assets/projects/project1.jpg';
+import projectImage2 from './assets/projects/project2.jpg';
+import projectImage3 from './assets/projects/project3.jpg';
+import projectImage4 from './assets/projects/project4.jpg';
+import projectImage5 from './assets/projects/project5.jpg';
+import projectImage6 from './assets/projects/project6.jpg';
+
+// Logos
+export const logos = {
+	logogradient: logogradient,
+	logo: logo,
+};
+
+// Enter your Personal Details here
+export const personalDetails = {
+	name: 'Nofrialdi',
+	tagline: 'I build things for web',
+	img: profile,
+	about: `Experienced Software Developer with expertise in design, installation, testing and maintenance of software systems. Equipped with a diverse and promising skill-set. Proficient in various platforms, languages, and embedded systems. Experienced with cutting-edge development tools and procedures. Able to effectively self-manage during independent projects, as well as collaborate as part of a productive team.`,
+};
+
+// Enter your Social Media URLs here
+export const socialMediaUrl = {
+	linkdein: 'https://www.linkedin.com/',
+	github: 'https://www.github.com/',
+	twitter: 'https://twitter.com/',
+	instagram: 'https://www.instagram.com/',
+};
+
+// Enter your Work Experience here
+export const workDetails = [
+	{
+		Position: 'Assistant Instructure',
+		Company: `Cv. Bina Kreasi Informatika`,
+		Location: 'Bekasi',
+		Type: 'Full Time',
+		Duration: 'Jan 2018 - Nov 2019',
+	},
+	{
+		Position: 'Android Developer',
+		Company: `PT. Fedora`,
+		Location: 'Jakarta',
+		Type: 'Full Time',
+		Duration: 'Sep 2016 - Dec 2017',
+	},
+	{
+		Position: 'Assistant Professor',
+		Company: `STMIK Jayakarta`,
+		Location: 'Jakarta',
+		Type: 'Full Time',
+		Duration: 'Aug 2015 - Jul 2016',
+	},
+];
+
+// Enter your Education Details here
+export const eduDetails = [
+	{
+		Position: 'Bachelor Degree in Information System',
+		Company: 'STMIK Jayakarta',
+		Location: 'Jakarta',
+		Duration: 'Jan 2013 - Apr 2017',
+	},
+	{
+		Position: 'High School',
+		Company: `SMA Negeri 11`,
+		Location: 'Ambon',
+		Duration: 'Jan 2009 - May 2012',
+	},
+];
+
+// Tech Stack and Tools
+export const techStackDetails = {
+	html: html,
+	css: css,
+	js: js,
+	react: react,
+	tailwind: tailwind,
+	bootstrap: bootstrap,
+	vscode: vscode,
+	postman: postman,
+	npm: npm,
+	git: git,
+	github: github,
+	figma: figma,
+};
+
+// Enter your Project Details here
+export const projectDetails = [
+	{
+		title: 'Project title 1',
+		image: projectImage1,
+		description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+		techstack: 'HTML/CSS, JavaScript',
+		previewLink: 'https://google.com',
+		githubLink: 'https://github.com',
+	},
+	{
+		title: 'Project title 2',
+		image: projectImage2,
+		description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+		techstack: 'HTML/CSS, JavaScript',
+		previewLink: 'https://google.com',
+		githubLink: 'https://github.com',
+	},
+	{
+		title: 'Project title 3',
+		image: projectImage3,
+		description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+		techstack: 'HTML/CSS, JavaScript',
+		previewLink: 'https://google.com',
+		githubLink: 'https://github.com',
+	},
+	{
+		title: 'Project title 4',
+		image: projectImage4,
+		description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+		techstack: 'HTML/CSS, JavaScript',
+		previewLink: 'https://google.com',
+		githubLink: 'https://github.com',
+	},
+	{
+		title: 'Project title 5',
+		image: projectImage5,
+		description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+		techstack: 'HTML/CSS, JavaScript',
+		previewLink: 'https://google.com',
+		githubLink: 'https://github.com',
+	},
+	{
+		title: 'Project title 6',
+		image: projectImage6,
+		description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+		techstack: 'HTML/CSS, JavaScript',
+		previewLink: 'https://google.com',
+		githubLink: 'https://github.com',
+	},
+];
+
+// Enter your Contact Details here
+export const contactDetails = {
+	email: 'aldi.fasive@gmail.com',
+	phone: '+62 89612022458',
+};
